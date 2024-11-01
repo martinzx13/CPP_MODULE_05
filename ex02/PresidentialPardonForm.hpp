c7abb1456dad39ba39d05dfab45285fa4de8bf09
Juan Aldana <juan.martinez43@udea.edu.co>
2024-11-01 12:10:06 +0000

    (create) pardom form logic

--- ex02/PresidentialPardonForm.hpp
+++ ex02/PresidentialPardonForm.hpp
@@ -7,8 +7,25 @@
 #include "AForm.hpp"
 
 class PresidentialPardonForm : public AForm {
-    private:
-    public:
+private:
+  std::string const _target;
+
+public:
+  //_________________________ Canonical Form.
+
+  PresidentialPardonForm();
+  PresidentialPardonForm(const std::string &target);
+  PresidentialPardonForm(const PresidentialPardonForm &_other);
+  ~PresidentialPardonForm();
+  //_________________________ Overload Operator.
+
+  PresidentialPardonForm &operator=(const PresidentialPardonForm &_other);
+
+  //_________________________ Getter.
+  std::string getTarget() const;
+
+  //_________________________ Excecution.
+  void execute(const Bureaucrat &excutor) const;
 };
 
 #endif
